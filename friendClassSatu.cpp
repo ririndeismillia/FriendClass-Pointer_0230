b5c92e78d3df6ea663c197d91471baf4898180da
ririndeismillia <ririndeismillia@gmail.com>
2026-04-27 18:15:25 +0700

    void siswa orang

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -23,4 +23,17 @@ public:
    void displayAll(orang &a);
 };
 
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+
 
