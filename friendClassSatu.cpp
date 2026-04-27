c2158ed361d726093ecc030a54243b5945206995
ririndeismillia <ririndeismillia@gmail.com>
2026-04-27 18:13:32 +0700

    friendClassSatu private&publik

---
+++ friendClassSatu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang
+{
+private:
+   string nama;
+
+public:
+   void setNama(string pNama);
+   friend class siswa;   
+};
+
+
+
